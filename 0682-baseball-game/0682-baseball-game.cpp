08d601f4a75f7e1e245c810c024c6de39246ac41
Sneh Awasthi <152710288+snehawasthi001@users.noreply.github.com>
2026-07-03 13:47:44 +0530

    Time: 3 ms (12.41%), Space: 12.9 MB (33.29%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<int> st;
+
+        for (string op : operations) {
+            if (op == "+") {
+                int top1 = st.top();
+                st.pop();
+                int top2 = st.top();
+
+                st.push(top1);
+                st.push(top1 + top2);
+            }
+            else if (op == "D") {
+                st.push(2 * st.top());
+            }
+            else if (op == "C") {
+                st.pop();
+            }
+            else {
+                st.push(stoi(op));
+            }
+        }
+
+        int sum = 0;
+        while (!st.empty()) {
+            sum += st.top();
+            st.pop();
+        }
+
+        return sum;
+    }
+};
